fd88cbc3bdbb6a62924c1f18e824d161777fd2f0
kimej2159 <kimej2159@naver.com>
2022-12-01 17:49:49 +0900

    2022.12.01 6장 끝

--- 2022-11-29-12일자.sql
+++ 2022-11-29-12일자.sql
@@ -314,6 +314,7 @@ SELECT *
 FROM month_salary;
 
 ROLLBACK;
+COMMIT;
 
 --5. 다중컬럼 서브 쿼리로 EMP_CNT(사원수), SUM_SALARY(급여합), AVG_SALARY(급여 평균)을 NULL 인 값들을 갱신
 UPDATE month_salary m
@@ -373,7 +374,7 @@ set EMP_CNT = ( SELECT COUNT(*)
     AVG_SALARY = ( SELECT ROUND(AVG(salary))
                 FROM employees
                 where department_id IS NULL)
-
+WHERE   dept_id IS NULL;
 --Kimberey의 정보를 조회
 select salary
 from employees
@@ -428,6 +429,212 @@ where EXISTS ( SELECT department_id
                             from employees d
                             where manager_id IS NOT NULL
                             AND e.department_id = d.department_id);                            
-                            
-                            
+
+--FROM      테이블1, 테이블2, ,,,         : 인라인 뷰 서브쿼리 위치
+--WHERE 조건들                           : 일반적인 서브쿼리 위치
+
+--6.5 스칼라 서브쿼리
+-- select 절에 사용하는 서브쿼리 형태
+-- 코드성 테이블에서 코드명을 조회하거나, 그룹 함수의 결과 값을 조회할 때 사용한다.
+-- ST_CLERK                     ,AVG(salary)
+
+[예제 6-22] 사원의 이름, 급여, 부서, 부서명 정보를 조회한다
+-- 서브쿼리로 조회
+select e.first_name, e.salary, e.department_id,
+        (select department_name
+         from departments d
+         where d.department_id = e.department_id)
+from employees e
+order by e.department_id;
+
+select first_name, salary, department_id,
+        (select round(avg(salary))
+        from employees) avg_sal
+from employees
+order by department_id;             // 서브 쿼리는 where 절에 쓰는게 일반적인데
+                                    // 여기서는 select 절에서 사용 됨
+desc employees;
+
+
+-- 일반 쿼리
+SELECT e.first_name, e.salary, 
+        d.department_id, d.department_name
+from employees e, departments d
+where e.department_id = d.department_id;
+
+select *
+from month_salary;              //이상함
+
+select dept_id sum_salary, avg_salary
+from month_salary;
+
+[연습문제6-3] 각 부서에 대해 부서코드, 부서명, 
+            부서가 위치한 도시이름을 조회하는 쿼리문 스칼라 수브쿼리를 사용하여 작성한다.
+
+select d.department_id, d.department_name
+from departments d
+order by 1;
+
+-- 일반 쿼리
+select d.department_id, d.department_name,
+        l.city
+from departments d INNER JOIN locations l
+using (location_id)
+order by 1;
+
+--6.6 인라인 뷰(객체) <------------> 서브쿼리의 (작성 위치에 따른) 구분
+
+--뷰(view)                   vs    테이블(table)
+--임시성 테이블                       영구적인 테이블
+--쿼리실행 생성~종료하면 사라짐          계속 살아있음
+--메모리                             저장 장치(물리적)
+
+-- ex) 오르클 설치시 미리 만들어준 emp_details_view를 조회 (= a.k.a 테이블)
+-- 모든 테이블의 데이터를 개발자에게 주지 않기 위한것
+desc emp_details_view;
+select * from emp_details_view; 
+
+----------------------------------------------------------------------
+-- 인라인 뷰 서브쿼리는 서브쿼리다! (뷰라는 객체로 구분하기 보다는, 서브 쿼리로 구분)
+-----------------------------------------------------------------------
+
+[예제 6-24] 급여가 회사 평균 급여 이상이고, 최대급여 이하인 사원의 사번, 이름, 급여, 회사평균 급여 , 회사최대급여를 조회한다.
+-- 일반 쿼리
+1. 평균 급여
+select round(avg(salary))
+from employees;                 -- 6462
+
+2.최대
+select max(salary) max_sal
+from employees;                 -- 24000
+       
+-- 1,2번 합쳐서       
+select  employee_id, first_name, salary   -- 급여 최대 최소 조회 불가능, 테이블이 없으니까
+from employees
+where salary >= 6462
+and   salary <= 24000;
+
+--회사 평균 급여, 회사최대 급여 컬럼이 존재하지 않음 (employees 와 그어떤 테이블도)
+-- 인라인 뷰 서브쿼리로, 마치 그러한 테이블이 존재하듯이 (집계성 테이블, month_salary처럼) 데이터 조회
+ 
+
+-- 인라인 뷰 서브쿼리로 해결해 보자    
+select e.employee_id, e.last_name, e.salary,
+        k.avg_sal, k.max_sal
+from employees e,
+    (select round(avg(salary)) avg_sal, max(salary) max_sal
+    from employees) k
+where salary >=6462
+and salary <= 24000;
+        
+select e.employee_id, e.last_name, e.salary,
+        k.avg_sal, k.max_sal
+from employees e,
+    (select round(avg(salary)) avg_sal, max(salary) max_sal
+    from employees) k
+where salary between k.avg_sal AND k.max_sal;   
+
+[예제 6-25] 사원 테이블의 데이터를 기준으로 월별 입사 현황을 조회해 테이블을 출력
+-- 실제로는 테이블이 없음, 임의로 테이블을 만들어야함
+
+-- 결과행은 하나 
+-- 컬럼 수는 12 개
+-- 데이터의 사원수는 월별 입사자의 합계
+
+-- 오라클 설정 확인
+select *
+from v$nls_parameters;      --RR/MM/DD, KOREAN  --> 11(mm, MM) vs 11월 (MON, MONTH)
+
+SELECT TO_CHAR(hire_date, 'MONTH') month
+from employees;
+
+select DECODE(TO_CHAR(hire_date, 'mm'), '01', count(*), 0) "1월",
+         DECODE(TO_CHAR(hire_date, 'mm'), '02', count(*), 0) "2월",
+         DECODE(TO_CHAR(hire_date, 'mm'), '03', count(*), 0) "3월",
+         DECODE(TO_CHAR(hire_date, 'mm'), '04', count(*), 0) "4월",
+         DECODE(TO_CHAR(hire_date, 'mm'), '05', count(*), 0) "5월",
+         DECODE(TO_CHAR(hire_date, 'mm'), '06', count(*), 0) "6월",
+         DECODE(TO_CHAR(hire_date, 'mm'), '07', count(*), 0) "7월",
+         DECODE(TO_CHAR(hire_date, 'mm'), '08', count(*), 0) "8월",
+         DECODE(TO_CHAR(hire_date, 'mm'), '09', count(*), 0) "9월",
+         DECODE(TO_CHAR(hire_date, 'mm'), '10', count(*), 0) "10월",
+         DECODE(TO_CHAR(hire_date, 'mm'), '11', count(*), 0) "11월",
+         DECODE(TO_CHAR(hire_date, 'mm'), '12', count(*), 0) "12월"
+from employees
+group by TO_CHAR(hire_date, 'mm')
+ORDER BY TO_CHAR(hire_date,'mm');
+
+[예제 6-26]
+select sum(m1) "1월",sum(m2) "2월",sum(m3) "3월",sum(m4) "4월",sum(m5) "5월", sum(m6) "6월", 
+        sum(m7) "7월",sum(m8) "8월",sum(m9) "9월",sum(m10) "10월",sum(m11) "11월", sum(m12) "12월"
+     
+from (
+        select DECODE(TO_CHAR(hire_date, 'mm'), '01', count(*),0) m1,
+                 DECODE(TO_CHAR(hire_date, 'mm'), '02', count(*),0) m2,
+                 DECODE(TO_CHAR(hire_date, 'mm'), '03', count(*),0) m3,
+                 DECODE(TO_CHAR(hire_date, 'mm'), '04', count(*),0) m4,
+                 DECODE(TO_CHAR(hire_date, 'mm'), '05', count(*),0) m5,
+                 DECODE(TO_CHAR(hire_date, 'mm'), '06', count(*),0) m6,
+                 DECODE(TO_CHAR(hire_date, 'mm'), '07', count(*),0) m7,
+                 DECODE(TO_CHAR(hire_date, 'mm'), '08', count(*),0) m8,
+                 DECODE(TO_CHAR(hire_date, 'mm'), '09', count(*),0) m9,
+                 DECODE(TO_CHAR(hire_date, 'mm'), '10', count(*),0) m10,
+                 DECODE(TO_CHAR(hire_date, 'mm'), '11', count(*),0) m11,
+                 DECODE(TO_CHAR(hire_date, 'mm'), '12', count(*),0) m12
+        from employees
+        group by TO_CHAR(hire_date, 'mm')
+        );
+
+-------------------------------------------------
+--ROWNUM은 서브쿼리가 아닌 분석 함수의 종류
+
+--ROWNUM vs ROW NUMBER()
+--의사컬럼 VS 의사컬럼 함수
+--표현식에 따른 
+--키보드  F1 눌러서 도움말에서 검색해 보세요
+
+
+[예제6-27]사번, 이름을 10건 조회한다
+select rownum num, employee_id,first_name
+from employees
+where rownum < 11
+order by 2;
+
+--순위(ranking)를 나타내는 함수
+-- RANK()       VS DENSE_RANK()
+-- ex) 급여 순위 1~10등 까지 
+
+
+select employee_id,first_name,
+        RANK() OVER (ORDER BY salary DESC) sal_rank1,
+        DENSE_RANK() OVER (ORDER BY salary DESC) sal_rank2,
+        ROW_NUMBER() OVER (ORDER BY salary DESC) sal_num1
+from employees;        
+
+select employee_id,first_name,
+        RANK() OVER (ORDER BY salary DESC) sal_rank1,
+        DENSE_RANK() OVER (ORDER BY salary DESC) sal_rank2,
+  --      AVERAGE_RANK() OVER (ORDER BY salary DESC) avg_rank3,
+        ROW_NUMBER() OVER (ORDER BY salary DESC) sal_num1
+from employees;     
+
+select employee_id,first_name,
+     AVERAGE_RANK() OVER (ORDER BY salary DESC) avg_rank3
+from employees;                         -- 안돼!
+
+[예제6-28]분석함수를 사용해서 급여가 높은 상위 10명 사원의 사번, 이름, 급여를 조회
+select employee_id, last_name, salary,
+       DENSE_RANK() OVER (ORDER BY salary DESC) sal_rank
+from employees
+where ROWNUM <= 10;
+
+-----------------------------------
+-- AVERAGE_RANK() 는 미확인
+
+
+
+
+
+
+
                             
